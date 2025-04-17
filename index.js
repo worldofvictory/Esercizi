6d02178150b3b71465154a3846b21ae655704d53
worldofvictory <133643234+worldofvictory@users.noreply.github.com>
2025-04-17 14:35:46 +0200

    countAppJS

---
+++ index.js
@@ -0,0 +1,15 @@
+
+let count = 110
+const countHeader = document.getElementById("count")
+const countAddBtn = document.getElementById("add")
+const countSubtractBtn = document.getElementById("subtract")
+countHeader.innerText = count
+
+countAddBtn.addEventListener("click", ()=> {
+    count++
+    countHeader.innerText = count
+})
+countSubtractBtn.addEventListener("click", ()=>{
+    count--
+    countHeader.innerText = count
+})
